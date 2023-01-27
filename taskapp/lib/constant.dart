0dec19e8f5c8bb1117554061ddfe5e99e4a8abdf
MCDiYae <mohammeddiyae.chbalou@etu.uae.ac.ma>
2023-01-27 18:42:01 +0100

    2 commit

---
+++ taskapp/lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const pPremuimColor = Color.fromARGB(255, 65, 41, 199);
+const pWriteColor1 = Colors.white;
--- taskapp/lib/view/homeview.dart
+++ taskapp/lib/view/homeview.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:taskapp/constant.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: ViewBody(),
+      
+    );
+  }
+}
+
+class AppBarView extends StatelessWidget {
+  const AppBarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ViewBody extends StatelessWidget {
+  const ViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
